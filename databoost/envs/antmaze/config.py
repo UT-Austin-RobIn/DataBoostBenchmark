eac388526f3636a01e904c342a1f57668f9aea4b
jullian-yapeter <jullianyapeter@gmail.com>
2022-11-16 17:27:27 -0800

    Fixed directionality and added top left as a task

--- databoost/envs/antmaze/config.py
+++ databoost/envs/antmaze/config.py
@@ -15,6 +15,27 @@ env_root = "/home/jullian-yapeter/code/DataBoostBenchmark/databoost/envs/antmaze
 
 
 '''Tasks configs'''
+# Mazes for reference
+'''
+BIG_MAZE = [[1, 1, 1, 1, 1, 1, 1, 1],
+            [1, G, 0, 1, 1, 0, G, 1],
+            [1, 0, 0, 1, 0, 0, 0, 1],
+            [1, 1, 0, 0, 0, 1, 1, 1],
+            [1, 0, 0, 1, 0, 0, 0, 1],
+            [1, 0, 1, 0, 0, 1, 0, 1],
+            [1, G, 0, 0, 1, 0, G, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1]]
+
+HARDEST_MAZE = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+                [1, G, 0, 0, 0, 1, 0, 0, 0, 0, G, 1],
+                [1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
+                [1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+                [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+                [1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 1],
+                [1, G, 0, 1, 0, 0, 0, 1, 0, 0, G, 1],
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+'''
 common_task_kwargs = {
     'reward_type':'sparse',
     'non_zero_reset': True,
@@ -30,27 +51,27 @@ common_expert_policy_kwargs = {
     "action_dim": 8
 }
 tasks = {
-    "medium-goal-bottom-left": AttrDict({
-        "task_name": "medium-goal-bottom-left",
-        "env": AntMazeEnv,
-        "env_kwargs": {
-            **copy.deepcopy(common_task_kwargs),
-            "maze_map": maze_env.BIG_MAZE,
-            "target_cell": (6, 6)
-        },
-        "seed_dataset": os.path.join(env_root, "data/seed/medium-goal-bottom-left"),
-        "expert_policy": TanhGaussianPolicy,
-        "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
-    }),
     "medium-goal-top-left": AttrDict({
         "task_name": "medium-goal-top-left",
         "env": AntMazeEnv,
         "env_kwargs": {
             **copy.deepcopy(common_task_kwargs),
             "maze_map": maze_env.BIG_MAZE,
-            "target_cell": (1, 6)
+            "target_cell": (6, 6)
         },
         "seed_dataset": os.path.join(env_root, "data/seed/medium-goal-top-left"),
+        "expert_policy": TanhGaussianPolicy,
+        "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
+    }),
+    "medium-goal-bottom-left": AttrDict({
+        "task_name": "medium-goal-bottom-left",
+        "env": AntMazeEnv,
+        "env_kwargs": {
+            **copy.deepcopy(common_task_kwargs),
+            "maze_map": maze_env.BIG_MAZE,
+            "target_cell": (1, 6)
+        },
+        "seed_dataset": os.path.join(env_root, "data/seed/medium-goal-bottom-left"),
         "expert_policy": TanhGaussianPolicy,
         "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
     }),
@@ -66,15 +87,15 @@ tasks = {
         "expert_policy": TanhGaussianPolicy,
         "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
     }),
-    "large-goal-bottom-left": AttrDict({
-        "task_name": "large-goal-bottom-left",
+    "medium-goal-bottom-right": AttrDict({
+        "task_name": "medium-goal-bottom-right",
         "env": AntMazeEnv,
         "env_kwargs": {
             **copy.deepcopy(common_task_kwargs),
-            "maze_map": maze_env.HARDEST_MAZE,
-            "target_cell": (1, 10)
+            "maze_map": maze_env.BIG_MAZE,
+            "target_cell": (1, 1)
         },
-        "seed_dataset": os.path.join(env_root, "data/seed/large-goal-bottom-left"),
+        "seed_dataset": os.path.join(env_root, "data/seed/medium-goal-bottom-right"),
         "expert_policy": TanhGaussianPolicy,
         "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
     }),
@@ -90,6 +111,18 @@ tasks = {
         "expert_policy": TanhGaussianPolicy,
         "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
     }),
+    "large-goal-bottom-left": AttrDict({
+        "task_name": "large-goal-bottom-left",
+        "env": AntMazeEnv,
+        "env_kwargs": {
+            **copy.deepcopy(common_task_kwargs),
+            "maze_map": maze_env.HARDEST_MAZE,
+            "target_cell": (1, 10)
+        },
+        "seed_dataset": os.path.join(env_root, "data/seed/large-goal-bottom-left"),
+        "expert_policy": TanhGaussianPolicy,
+        "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
+    }),
     "large-goal-top-right": AttrDict({
         "task_name": "large-goal-top-right",
         "env": AntMazeEnv,
@@ -101,21 +134,35 @@ tasks = {
         "seed_dataset": os.path.join(env_root, "data/seed/large-goal-top-right"),
         "expert_policy": TanhGaussianPolicy,
         "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
-    })
+    }),
+    "large-goal-bottom-right": AttrDict({
+        "task_name": "large-goal-bottom-right",
+        "env": AntMazeEnv,
+        "env_kwargs": {
+            **copy.deepcopy(common_task_kwargs),
+            "maze_map": maze_env.HARDEST_MAZE,
+            "target_cell": (1, 1)
+        },
+        "seed_dataset": os.path.join(env_root, "data/seed/large-goal-bottom-right"),
+        "expert_policy": TanhGaussianPolicy,
+        "expert_policy_kwargs": copy.deepcopy(common_expert_policy_kwargs)
+    }),
 }
 
 
 '''Prior tasks configs'''
 prior_tasks_list = [
     "medium-goal-top-right",
+    "medium-goal-top-left",
     "medium-goal-bottom-right",
     "medium-goal-bottom-left",
     "large-goal-top-right",
+    "large-goal-top-left",
     "large-goal-bottom-right",
     "large-goal-bottom-left"
 ]
 prior_dataset_dir = os.path.join(env_root, "data/prior")
-prior_n_demos = 20
+prior_n_demos = 10
 prior_do_render = True
 prior_action_noise_pct = 0.1
 prior_imgs_res = (224, 224)
@@ -129,12 +176,7 @@ prior_dataset_kwargs = AttrDict({
 
 '''Seed tasks configs'''
 seed_tasks_list = [
-    "medium-goal-top-right",
-    "medium-goal-bottom-right",
-    "medium-goal-bottom-left",
-    "large-goal-top-right",
-    "large-goal-bottom-right",
-    "large-goal-bottom-left"
+    "medium-goal-top-right"
 ]
 seed_dataset_dir = os.path.join(env_root, "data/seed")
 seed_n_demos = 10
