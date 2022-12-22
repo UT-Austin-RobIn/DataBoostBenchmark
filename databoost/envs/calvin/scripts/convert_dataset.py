9de9665c545c2bd982173905627b5c5e9a31340d
jullian-yapeter <jullianyapeter@gmail.com>
2022-12-22 15:00:35 -0800

    WIP calvin data conversion

--- databoost/envs/calvin/scripts/convert_dataset.py
+++ databoost/envs/calvin/scripts/convert_dataset.py
@@ -1,6 +1,11 @@
 import os
 import numpy as np
+from typing import Dict
 
+from tqdm import tqdm
+
+from databoost.utils.general import AttrDict
+from databoost.envs.calvin.utils import render
 from databoost.envs.calvin.custom import CalvinEnv
 
 
@@ -14,12 +19,20 @@ def npz_files(dir_path):
 def get_scene_id(scene_intervals, ep_id):
     for scene_id, scene_interval in scene_intervals.items():
         start, end = scene_interval
-        if ep_id >= start and ep_id < end:
+        if ep_id >= start and ep_id <= end:
             return scene_id
     return None
 
 
-def init_traj() -> Dict:
+def get_traj_id(ep_start_end_ids, ep_id):
+    for traj_id, traj_interval in enumerate(ep_start_end_ids):
+        start, end = traj_interval
+        if ep_id >= start and ep_id <= end:
+            return traj_id
+    return None
+
+
+def init_traj() -> AttrDict:
     traj = {}
     for attr in [
             "observations",
@@ -30,56 +43,56 @@ def init_traj() -> Dict:
             "imgs"
         ]:
         traj[attr] = [] if attr != "infos" else {}
-    return traj
+    return AttrDict(traj)
 
 
-# def add_to_traj(traj: AttrDict,
-#                 ob: np.ndarray,
-#                 act: np.ndarray,
-#                 rew: float,
-#                 done: bool,
-#                 info: Dict,
-#                 im: np.ndarray = None):
-#     '''helper function to append a step's results to a trajectory dictionary
+def add_to_traj(traj: AttrDict,
+                ob: np.ndarray,
+                act: np.ndarray,
+                rew: float,
+                done: bool,
+                info: Dict,
+                im: np.ndarray = None):
+    '''helper function to append a step's results to a trajectory dictionary
 
-#     Args:
-#         traj [AttrDict]: dictionary with keys {
-#             observations, actions, rewards, dones, infos, imgs}
-#         ob [np.ndarray]: env-specific observation
-#         act [np.ndarray]: env-specific action
-#         rew [float]: reward; float
-#         done [bool]: done flag
-#         info [Dict]: task-specific info
-#         im [np.ndarray]: rendered image after the step
-#     '''
-#     traj.observations.append(ob)
-#     traj.actions.append(act)
-#     traj.rewards.append(rew)
-#     traj.dones.append(done)
-#     for attr in info:
-#         if attr not in traj.infos:
-#             traj.infos[attr] = []
-#         traj.infos[attr].append(info[attr])
-#     if im is not None:
-#         traj.imgs.append(im)
+    Args:
+        traj [AttrDict]: dictionary with keys {
+            observations, actions, rewards, dones, infos, imgs}
+        ob [np.ndarray]: env-specific observation
+        act [np.ndarray]: env-specific action
+        rew [float]: reward; float
+        done [bool]: done flag
+        info [Dict]: task-specific info
+        im [np.ndarray]: rendered image after the step
+    '''
+    traj.observations.append(ob)
+    traj.actions.append(act)
+    traj.rewards.append(rew)
+    traj.dones.append(done)
+    for attr in info:
+        if attr not in traj.infos:
+            traj.infos[attr] = []
+        traj.infos[attr].append(info[attr])
+    if im is not None:
+        traj.imgs.append(im)
 
 
-# def traj_to_numpy(self, traj: AttrDict) -> AttrDict:
-#     '''convert trajectories attributes into numpy arrays
+def traj_to_numpy(traj: AttrDict) -> AttrDict:
+    '''convert trajectories attributes into numpy arrays
 
-#     Args:
-#         traj [AttrDict]: dictionary with keys {obs, acts, rews, dones, infos, ims}
-#     Returns:
-#         traj_numpy [AttrDict]: trajectory dict with attributes as numpy arrays
-#     '''
-#     traj_numpy = self.init_traj()
-#     for attr in traj:
-#         if attr not in ("info", "infos"):
-#             traj_numpy[attr] = np.array(traj[attr])
-#         else:
-#             for info_attr in traj.infos:
-#                 traj_numpy.infos[info_attr] = np.array(traj.infos[info_attr])
-#     return traj_numpy
+    Args:
+        traj [AttrDict]: dictionary with keys {obs, acts, rews, dones, infos, ims}
+    Returns:
+        traj_numpy [AttrDict]: trajectory dict with attributes as numpy arrays
+    '''
+    traj_numpy = init_traj()
+    for attr in traj:
+        if attr not in ("info", "infos"):
+            traj_numpy[attr] = np.array(traj[attr])
+        else:
+            for info_attr in traj.infos:
+                traj_numpy.infos[info_attr] = np.array(traj.infos[info_attr])
+    return traj_numpy
 
 
 def get_skill(skill_data, ep_id):
@@ -89,7 +102,7 @@ def get_skill(skill_data, ep_id):
             mid = (high + low) // 2
             # If element is present at the middle itself
             start, end, skill = skill_data[mid]
-            if ep_id >= start and ep_id < end:
+            if ep_id >= start and ep_id <= end:
                 return skill
             # If element is smaller than mid, then it can only
             # be present in left subarray
@@ -114,9 +127,11 @@ if __name__=="__main__":
     scene_intervals = np.load(os.path.join(data_root,
                                            "training/scene_info.npy"),
                               allow_pickle=True).item()
+    # scene_intervals is a dict of {A: [0, 611098], B: [611099, 1210008], ... }
     scene_intervals = {scene.split("_")[-1]: interval
                        for scene, interval in scene_intervals.items()}
-    ep_start_end_ids = np.load(os.path.join(data_dir, "ep_start_end_ids.npy")) 
+    ep_start_end_ids = np.load(os.path.join(data_dir, "ep_start_end_ids.npy"))
+    print(ep_start_end_ids)
     skill_data = np.load(os.path.join(data_dir,
                                       "lang_annotations/auto_lang_ann.npy"),
                          allow_pickle=True).item()
@@ -126,19 +141,25 @@ if __name__=="__main__":
     skill_data = sorted(skill_data)
     
     # iterate through episode step files and build the episode trajectory
-    curr_ep_idx = 0
+    curr_traj_id = 0
     curr_skill_idx = 0
     traj_data = init_traj()
-    for ep_step_path in npz_files(data_dir):
+    for ep_step_path in tqdm(npz_files(data_dir)):
         # convert "path/to/dir/episode_0001234.npz" to 1234
         ep_id = int(os.path.splitext(os.path.split(ep_step_path)[-1].split("_")[-1])[0])
         ep_step_data = np.load(ep_step_path)
-        # identify the scene, skill
+        # identify the traj id, scene, skill
+        traj_id = get_traj_id(ep_start_end_ids, ep_id)
         scene_id = get_scene_id(scene_intervals, ep_id)
         skill = get_skill(skill_data, ep_id)
         # get step data
-        print(ep_id, scene_id, skill)
-
-
-
-
+        env.reset(robot_obs=ep_step_data["robot_obs"],
+                  scene_obs=ep_step_data["scene_obs"])
+        ob = np.concatenate([ep_step_data["robot_obs"],
+                             ep_step_data["scene_obs"]])
+        im = render(env)
+        act = ep_step_data["rel_actions"]
+        rew = 0
+        done = 0
+        _, _, _, info = env.step(act)
+        add_to_traj(traj_data, act, rew, done, info, im)
