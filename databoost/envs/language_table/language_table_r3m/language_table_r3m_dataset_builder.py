e6911f023ce0287d3acad66d52f18cb23e800126
Karl Pertsch <kpertsch@google.com>
2023-01-29 18:25:17 -0800

    update dataset builder

--- databoost/envs/language_table/language_table_r3m/language_table_r3m_dataset_builder.py
+++ databoost/envs/language_table/language_table_r3m/language_table_r3m_dataset_builder.py
@@ -1,7 +1,15 @@
 """language_table_r3m dataset."""
 
 import tensorflow_datasets as tfds
+import tensorflow as tf
+import torch
+from r3m import load_r3m
 
+SUBSEQ_LEN = 1
+R3M_FEATURE_SIZE = 2048
+ACTION_DIM = 2
+
+ORIG_DATASET_PATH = "gs://gresearch/robotics/language_table_sim"
 
 class Builder(tfds.core.GeneratorBasedBuilder):
   """DatasetBuilder for language_table_r3m dataset."""
@@ -11,37 +19,56 @@ class Builder(tfds.core.GeneratorBasedBuilder):
       '1.0.0': 'Initial release.',
   }
 
+  def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+      self.r3m = load_r3m("resnet50")  # resnet18, resnet34
+      self.r3m.eval()
+      self.r3m.to(self.device)
+
   def _info(self) -> tfds.core.DatasetInfo:
     """Returns the dataset metadata."""
-    # TODO(language_table_r3m): Specifies the tfds.core.DatasetInfo object
     return self.dataset_info_from_configs(
         features=tfds.features.FeaturesDict({
             # These are the features of your dataset like images, labels ...
-            'image': tfds.features.Image(shape=(None, None, 3)),
-            'label': tfds.features.ClassLabel(names=['no', 'yes']),
+            'observations': tfds.features.Tensor(shape=(SUBSEQ_LEN, R3M_FEATURE_SIZE), dtype=tf.float32),
+            'actions': tfds.features.Tensor(shape=(SUBSEQ_LEN, ACTION_DIM), dtype=tf.float32),
         }),
-        # If there's a common (input, target) tuple from the
-        # features, specify them here. They'll be used if
-        # `as_supervised=True` in `builder.as_dataset`.
-        supervised_keys=('image', 'label'),  # Set to `None` to disable
+        supervised_keys=None,
         homepage='https://dataset-homepage/',
     )
 
   def _split_generators(self, dl_manager: tfds.download.DownloadManager):
     """Returns SplitGenerators."""
-    # TODO(language_table_r3m): Downloads the data and defines the splits
-    path = dl_manager.download_and_extract('https://todo-data-url')
+    builder = tfds.builder_from_directory(os.path.join(ORIG_DATASET_PATH, '0.0.1'))
 
     # TODO(language_table_r3m): Returns the Dict[split names, Iterator[Key, Example]]
     return {
-        'train': self._generate_examples(path / 'train_imgs'),
+        'train': self._generate_examples(builder.as_dataset(split='train[:10]'), 'train'),
+        # 'val': self._generate_examples(builder.as_dataset(split='val'), 'val'),
+        # 'test': self._generate_examples(builder.as_dataset(split='test'), 'test'),
     }
 
-  def _generate_examples(self, path):
+  def _generate_examples(self, ds, split):
     """Yields examples."""
-    # TODO(language_table_r3m): Yields (key, example) tuples from the dataset
-    for f in path.glob('*.jpeg'):
-      yield 'key', {
-          'image': f,
-          'label': 'yes',
-      }
+    for i, episode in enumerate(ds):
+        # extract raw observation-action pair
+        obs, act = [], []
+        instruct = None
+        for step in episode['steps']:
+            obs.append(step['observation']['rgb'])
+            act.append(step['action'])
+            if instruct is None:
+                instruct = step['observation']['instruction'].numpy()
+
+        # encode all observations with R3M
+        imgs = torch.from_numpy(tf.stack(obs).numpy().transpose(0, 3, 1, 2)).to(self.device)
+        encs = self.r3m(imgs).data.cpu().numpy()     # [seq_len, 2048]
+
+        # write windows to output
+        acts = tf.stack(act).numpy()                # [seq_len, 2]
+        for k in range(len(act) - SUBSEQ_LEN):
+            yield split + '_' + str(i) + '_' + str(k), {
+                'observations': encs[i : i+SUBSEQ_LEN],
+                'actions': acts[i : i+SUBSEQ_LEN],
+            }
