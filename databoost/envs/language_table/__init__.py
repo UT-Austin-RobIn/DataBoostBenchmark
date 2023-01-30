39d1cf0ac6488a983ee984dd4fa8a642b45d3921
Karl Pertsch <kpertsch@google.com>
2023-01-30 12:28:58 -0800

    prototype online RL for language table

--- databoost/envs/language_table/__init__.py
+++ databoost/envs/language_table/__init__.py
@@ -6,15 +6,24 @@ import gym
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
+import torch
 
 from databoost.base import DataBoostEnvWrapper, DataBoostBenchmarkBase
 from databoost.utils.general import AttrDict
 from language_table.environments import blocks
 from language_table.environments import language_table
 from language_table.environments.rewards import block2block
+from r3m import load_r3m
 
 
 class DataBoostBenchmarkLanguageTable(DataBoostBenchmarkBase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        self.r3m = load_r3m("resnet50")  # resnet18, resnet34
+        self.r3m.eval()
+        self.r3m.to(self.device)
 
     def get_env(self, task_name: str) -> DataBoostEnvWrapper:
         '''get the wrapped gym environment corresponding to the specified task.
@@ -27,6 +36,13 @@ class DataBoostBenchmarkLanguageTable(DataBoostBenchmarkBase):
                                        the corresponding seed and prior offline
                                        datasets
         '''
+        def language_table_postproc(obs, reward, done, info):
+            '''Encode image with r3m for observation.'''
+            img = torch.from_numpy(obs['rgb'].transpose(2, 0, 1)).to(self.device)[None]
+            img = torchvision.transforms.Resize((224, 224))(img)
+            obs = self.r3m(img).data.cpu().numpy()[0]  # [1, 2048]
+            return obs, reward, done, info
+
         env = language_table.LanguageTable(
             block_mode=blocks.LanguageTableBlockVariants.BLOCK_8,
             reward_factory=block2block.BlockToBlockReward,
@@ -38,6 +54,7 @@ class DataBoostBenchmarkLanguageTable(DataBoostBenchmarkBase):
                 prior_dataset_url="/data/karl/data/table_sim/prior_data",
                 test_dataset_url="/data/karl/data/table_sim/prior_data",
                 render_func=lambda x: x.render(),
+                postproc_func=language_table_postproc,
             )
         return env
 
@@ -61,70 +78,6 @@ class DataBoostBenchmarkLanguageTable(DataBoostBenchmarkBase):
             success [bool]: success flag
         '''
         return done and rew > 0
-
-
-class DataBoostEnvWrapperLanguageTable(DataBoostEnvWrapper):
-    '''Overwrites data loading with tfds data pipeline.'''
-
-    def _get_dataset(self, dataset_dir: str, n_demos: int = None) -> AttrDict:
-        raise NotImplementedError('Language Table does not support loading of whole dataset at once.')
-
-    def _get_dataloader(self,
-                        dataset_dir: str,
-                        n_demos: int = None,
-                        seq_len: int = None,
-                        batch_size: int = 1,
-                        shuffle: bool = True,
-                        load_imgs: bool = True,
-                        goal_condition: bool = False) -> Any: #DataLoader:
-        '''gets a dataloader to load in h5 data from the given dataset_dir.
-
-        Args:
-            dataset_dir [str]: directory of h5 data
-            n_demos [int]: the number of demos (h5 files) to retrieve from the
-                           dataset dir
-            seq_len [int]: the window length with which to split demonstrations
-            batch_size [int]: number of sequences to load in as a batch
-            shuffle [bool]: shuffle sequences to be loaded
-        Returns:
-            dataloader [DataLoader]: DataLoader for given dataset directory
-        '''
-        def prep_episode(episode):
-            """Samples random subsequence, extracts observation, builds output dict."""
-            # sample random start step
-            #steps = list(iter(episode['steps']))
-            #start_step = np.random.randint(0, len(steps) - seq_len - 1)
-
-            step = next(iter(episode['steps']))
-            return {'observations': tf.concat((step['observation']['effector_translation'],
-                                               step['observation']['effector_target_translation']), axis=-1)[None],
-                    'actions': step['action'][None],
-            }
-
-            obs, act = [], []
-            for step in episode['steps']: #steps[start_step : start_step + seq_len]:
-                obs.append(
-                    tf.concat((step['observation']['effector_translation'],
-                               step['observation']['effector_target_translation']), axis=-1))
-                act.append(step["action"])
-                if len(obs) == 2: break
-            return {
-                "observations": obs, #tf.stack(obs, axis=0),
-                "actions": act, #tf.stack(act, axis=0),
-            }
-
-        builder = tfds.builder_from_directory(os.path.join(dataset_dir, '0.0.1'))
-        ds = builder.as_dataset(split='train[:2560]')
-        ds = ds.cache()
-        #ds = ds.map(prep_episode)
-
-        if shuffle:
-            ds = ds.shuffle(1024)
-        ds = ds.batch(batch_size)
-        ds = ds.prefetch(tf.data.experimental.AUTOTUNE)
-        tfds.benchmark(ds)
-        tfds.benchmark(ds)
-        return tfds.as_numpy(ds)
 
 
 __all__ = [DataBoostBenchmarkLanguageTable]
