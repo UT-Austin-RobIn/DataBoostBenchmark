f8ff7442e00f31d928ecc2d50898a92fc5f2232a
jullian-yapeter <jullianyapeter@gmail.com>
2023-02-07 18:30:46 -0800

    WIP

--- databoost/__init__.py
+++ databoost/__init__.py
@@ -1,14 +1,14 @@
 from databoost.base import DataBoostBenchmarkBase
-from databoost.envs.language_table import DataBoostBenchmarkLanguageTable
+# from databoost.envs.language_table import DataBoostBenchmarkLanguageTable
 #from databoost.envs.antmaze import DataBoostBenchmarkAntMaze
 #from databoost.envs.calvin import DataBoostBenchmarkCalvin
-#from databoost.envs.metaworld import DataBoostBenchmarkMetaworld
+from databoost.envs.metaworld import DataBoostBenchmarkMetaworld
 
 
 # dictionary of DataBoost's benchmark names and corresponding benchmark objects
 benchmarks = {
-    "language_table": DataBoostBenchmarkLanguageTable,
- #   "metaworld": DataBoostBenchmarkMetaworld,
+    # "language_table": DataBoostBenchmarkLanguageTable,
+   "metaworld": DataBoostBenchmarkMetaworld,
  #   "antmaze": DataBoostBenchmarkAntMaze,
  #   "calvin": DataBoostBenchmarkCalvin
 }
